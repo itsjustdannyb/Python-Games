8d746f99800afe9a708d44051043f4c985801784
itsjustdannyb <108878876+itsjustdannyb@users.noreply.github.com>
2022-10-11 13:10:23 +0100

    Done with Madlibs game

--- Games/Madlibs.py
+++ Games/Madlibs.py
@@ -1,4 +1,21 @@
 # madlibs game
 # string cancatenation
 
-print("I am daniel Bamilosin")
+# youtuber = "Japadotnet"
+
+# Ways of Cancatenating.
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+place = input("Place: ")
+animal = input("Animal: ")
+country = input("Country: ")
+food = input("Food: ")
+sport = input("Sport: ")
+movie_villain = input("Movie Villain: ")
+
+# Variable that stores Mad libs
+madlib = f"Today, I had a dream I was lost in {place}\nI saw a female {animal} delivering some {food} to the president of {country}.\nThe president asked the {animal} If she would like to play {sport} on Mars.\nThe {animal} said sure, why not!\nThen out of nowhere {movie_villain} came and stole the {food}\n...oh and he burnt down the president's house.\nTHE END?"
+
+print(madlib)
